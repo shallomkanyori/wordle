adc79bba8df1624dd0878781e1722cdb75c07a58
Shallom Kanyori <shallomkanyori@gmail.com>
2024-05-16 11:22:10 +0000

    Add function to check guess

--- main.py
+++ main.py
@@ -6,8 +6,26 @@ from words import word_list
 from utils import Util
 
 WELCOME_MESSAGE = f'\n[white on blue] WELCOME TO WORDLE [/]\n'
+GUESS_STATEMENT = 'Enter your guess'
 INSTRUCTIONS = 'You may start guessing\n'
 ALLOWED_GUESSES = 6
+
+
+def check_guess(guess, answer):
+    """Checks a given guess"""
+
+    guessed = []
+
+    for i, letter in enumerate(guess):
+        if answer[i] == guess[i]:
+            guessed += Util.correct_place(letter)
+        elif letter in answer:
+            guessed += Util.correct_letter(letter)
+        else:
+            guessed += Util.incorrect_letter(letter)
+
+    return ''.join(guessed)
+
 
 if __name__ == '__main__':
     console = Console()
