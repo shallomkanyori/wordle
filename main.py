585375d427c29d4ab9390022dd60a0824933fa58
Shallom Kanyori <shallomkanyori@gmail.com>
2024-05-27 13:18:28 +0000

    Implement game loop

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from words import word_list
 from utils import Util
 
 WELCOME_MESSAGE = f'\n[white on blue] WELCOME TO WORDLE [/]\n'
-GUESS_STATEMENT = 'Enter your guess'
+GUESS_STATEMENT = 'Enter your guess: '
 INSTRUCTIONS = 'You may start guessing\n'
-ALLOWED_GUESSES = 6
+TRIES = 6
 
 
 def check_guess(guess, answer):
@@ -18,17 +18,63 @@ def check_guess(guess, answer):
 
     for i, letter in enumerate(guess):
         if answer[i] == guess[i]:
-            guessed += Util.correct_place(letter)
+            guessed.append(Util.correct_place(letter))
         elif letter in answer:
-            guessed += Util.correct_letter(letter)
+            guessed.append(Util.correct_letter(letter))
         else:
-            guessed += Util.incorrect_letter(letter)
+            guessed.append(Util.incorrect_letter(letter))
 
-    return ''.join(guessed)
+    return ' '.join(guessed)
+
+
+def game(console, word):
+    """Game loop
+
+    Args:
+        console: Rich console.
+        word (str): The word to guess.
+    """
+
+    playing = True
+    already_guessed = set()
+    all_guessed = []
+
+    while playing:
+        guess = input(GUESS_STATEMENT).upper()
+
+        while True:
+            if len(guess) != 5:
+                console.print('[red]Please enter a 5-letter word!!\n[/]')
+            elif guess in already_guessed:
+                console.print("[red]You've already guessed this word!!\n[/]")
+            else:
+                break
+
+            guess = input(GUESS_STATEMENT).upper()
+
+        already_guessed.add(guess)
+        guessed = check_guess(guess, word)
+        all_guessed.append(guessed)
+
+        console.print('')
+        console.print(*all_guessed, sep='\n\n', justify='center')
+        console.print('')
+
+        if guess == word or len(already_guessed) == TRIES:
+            playing = False
+
+    if guess != word:
+        console.print(f'\n[red]WORDLE X/{TRIES}[/]')
+        console.print(f'\n[green]Correct Word: {word}[/]')
+    else:
+        console.print(f'\n[green]WORDLE {len(already_guessed)}/{TRIES}[/]\n')
 
 
 if __name__ == '__main__':
-    console = Console()
+    console = Console(width=35)
     word = choice(word_list)
-    console.print(WELCOME_MESSAGE)
-    console.print(INSTRUCTIONS)
+
+    console.print(WELCOME_MESSAGE, justify='center')
+    console.print(INSTRUCTIONS, justify='center')
+
+    game(console, word)
--- utils.py
+++ utils.py
@@ -8,14 +8,14 @@ class Util:
     @staticmethod
     def correct_place(letter):
         """Returns a styled string for a letter placed correctly"""
-        return f'[black on green]{letter}[/]'
+        return f'[bold white on green] {letter} [/]'
 
     @staticmethod
     def correct_letter(letter):
         """Returns a styled string for a letter placed incorrectly"""
-        return f'[black on yellow]{letter}[/]'
+        return f'[bold white on yellow] {letter} [/]'
 
     @staticmethod
     def incorrect_letter(letter):
         """Returns a styled string for an incorrect letter"""
-        return f'[black on white]{letter}[/]'
+        return f'[bold white on grey39] {letter} [/]'
